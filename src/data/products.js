2f6bfecbcae8bbbed7316eaaf5680d35da217baa
Finical3D <matt@finical3d.co.nz>
2026-05-18 19:48:43 +1200

    adding shop functions

---
+++ src/data/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    slug: 'crystal-golem',
+    name: 'Crystal Golem',
+    price: 12.00,
+    description: 'Highly detailed 28mm crystal golem miniature...',
+    image: '/images/shop/crystal-golem.JPG',
+    material: 'Resin',
+    scale: '28mm',
+    inStock: true,
+    tags: ['Miniature', 'Sci-Fi', '28mm', 'Resin'],
+    stripeLink: 'https://buy.stripe.com/yourlink'
+  },
+  {
+    slug: 'dragon',
+    name: 'Dragon',
+    price: 25.00,
+    description: 'Powerful dragon miniature...',
+    image: '/images/shop/dragon.jpg',
+    material: 'Resin',
+    scale: '75mm',
+    inStock: true,
+    tags: ['Miniature', 'Fantasy', '75mm', 'Resin'],
+    stripeLink: 'https://buy.stripe.com/yourlink'
+  }
+]
